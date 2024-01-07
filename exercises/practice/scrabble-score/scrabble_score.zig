eb3bef45072c14e13e162d884d0bcdc60f41774b
Dominic London <25865081+djlondon@users.noreply.github.com>
2024-01-07 22:46:06 +0000

    naïve scrabble score

--- exercises/practice/scrabble-score/scrabble_score.zig
+++ exercises/practice/scrabble-score/scrabble_score.zig
@@ -1,4 +1,23 @@
 pub fn score(s: []const u8) u32 {
-    _ = s;
-    @compileError("please implement the score function");
+    var points: u32 = 0;
+    for (s) |value| {
+        points += switch (value) {
+            'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' => 1,
+            'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' => 1,
+            'D', 'G'                => 2,
+            'd', 'g'                => 2,
+            'B', 'C', 'M', 'P'      => 3,
+            'b', 'c', 'm', 'p'      => 3,
+            'F', 'H', 'V', 'W', 'Y' => 4,
+            'f', 'h', 'v', 'w', 'y' => 4,
+            'K'                     => 5,
+            'k'                     => 5,
+            'J', 'X'                => 8,
+            'j', 'x'                => 8,
+            'Q', 'Z'                => 10,
+            'q', 'z'                => 10,
+            else                    => 0,
+        };
+    }
+    return points;
 }
